dbcc4b00ea9d743525c49c0b86fd642318dec746
lalankeba <lalankeba@gmail.com>
2023-09-19 20:28:51 +0300

    adding init service to docs

---
+++ src/test/java/com/laan/sportsda/controller/IndexControllerTest.java
@@ -0,0 +1,51 @@
+package com.laan.sportsda.controller;
+
+import com.laan.sportsda.util.PathUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureRestDocs
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void init() throws Exception {
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get(PathUtil.INIT))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(
+                        document("{method-name}",
+                                preprocessResponse(prettyPrint())
+                        ));
+    }
+
+    @Test
+    void initInSi() throws Exception {
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get(PathUtil.INIT)
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "si")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(
+                        document("{method-name}",
+                                preprocessResponse(prettyPrint())
+                        )
+                );
+    }
+}
