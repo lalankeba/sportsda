8b1ede287be20f367f23cb8262789e9486ccc273
lalankeba <lalankeba@gmail.com>
2023-11-28 13:00:50 +0200

    updating to spring boot version to 3.2 and remove modelmapper

--- src/main/java/com/laan/sportsda/config/BeanConfig.java
+++ src/main/java/com/laan/sportsda/config/BeanConfig.java
@@ -1,7 +1,5 @@
 package com.laan.sportsda.config;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,14 +11,6 @@ import java.util.Locale;
 
 @Configuration
 public class BeanConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        modelMapper.getConfiguration().setDeepCopyEnabled(false);
-        return modelMapper;
-    }
 
     @Bean
     public LocaleResolver localeResolver() {
